b019eda391fa680c5520d99b747cac8522d8685b
Justin Ribeiro <justin@justinribeiro.com>
2019-10-15 18:00:41 -0700

    feat: roll some videos

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # devtools-to-video
-Output a video file from screenshot frames within a Chrome DevTools JSON trace file.
+
+> Output a video file from screenshot frames within a Chrome DevTools JSON trace file.
+
+![image](https://user-images.githubusercontent.com/643503/66878963-26095700-ef71-11e9-96e7-7977b4f73577.png)
+
+Example video: [justinribeiro.com @ 3G Slow, Moto G4](https://www.youtube.com/watch?v=guJLfqTFfIw)
+
+## Install
+
+```sh
+npm i @justinribeiro/devtools-to-video
+# or
+yarn global add @justinribeiro/devtools-to-video
+```
+
+After install, run the command with `-h` to see usage information:
+
+```sh
+➜ devtools-to-video -h
+
+
+  DEVTOOLS-TO-VIDEO
+  Output a video file from screenshot frames within a Chrome DevTools JSON trace file.
+  Usage: `devtools-to-video [options ...]`
+
+
+Global Options
+
+  -i, --input string    File path to Chrome DevTools trace JSON file.
+  -o, --output string   Output file name for video file.
+  -c, --hideClock       If set, hides the time scale clock on the output video file.
+  -l, --label string    If set, adds the label above the time scale clock in the output video file.
+  -f, --frames number   Sets the frames per second of the output video.
+  -h, --help            Print out helpful usage information for this program.
+  -v, --version         Print out current program version number.
+
+Version
+
+  0.1.0
+```
+
+## Sample command
+
+```sh
+➜ devtools-to-video -i Profile-20191015T174036.json -o sample5.mp4 -l '3G Slow @ Moto G4'
+ STARTING UP  Checking environment and setting up params.
+ CONVERT  Spawning FFMPEG worker with open pipe.
+ CONVERT  Begin piping screenshots from DevTools trace to FFMPEG.
+ SCREENSHOTS  Processed 24 screenshots into video file.
+ CONVERT  DevTools trace successfully converted to MP4.
+ LABEL PASS  Adding label and time scale to MP4.
+ LABEL PASS  Finished adding label and time scale to MP4.
+ COMPLETE  DevTools trace to video is now complete! You file sample5.mp4 is ready
+```
+
+## Credit where credit is due
+
+1. WebPageTest does this [way cooler](https://github.com/WPO-Foundation/webpagetest/blob/26e3cf0/www/video/render.php#L567).
+2. Kris Selden's [trace-to-mp4.js gist](https://gist.github.com/krisselden/bf98fb0c192fcb73ed32e79c0a7972d2) (mad props)
---
+++ src/run.js
@@ -0,0 +1,201 @@
+/* eslint-disable node/no-unpublished-require */
+'use strict';
+
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const tmp = require('tmp');
+const chalk = require('chalk');
+const colorGreen = chalk.default.bgGreen;
+const colorBlue = chalk.default.bgBlue;
+const colorRed = chalk.default.bgRedBright;
+const colorYellow = chalk.default.bgYellowBright;
+
+module.exports = function devtoolsToVideo(args) {
+  if (args.input === '') {
+    messageError('Input file not defined. Did you forget to set -i?');
+    throw new Error();
+  }
+
+  if (args.output === '') {
+    messageError('Output file not defined. Did you forget to set -o?');
+    throw new Error();
+  }
+
+  try {
+    if (fs.existsSync(args.input)) {
+      // just checking
+    }
+  } catch (error) {
+    messageError('Input file does not appear to exist.');
+    throw new Error();
+  }
+
+  let timeline;
+  try {
+    timeline = JSON.parse(fs.readFileSync(args.input, 'utf8'));
+  } catch (error) {
+    messageError(
+      `Cannot parse ${args.input}. Maybe it's not a Chrome DevTools JSON file?`,
+    );
+    throw new Error();
+  }
+
+  const screenshots = timeline.filter(event => event.name === 'Screenshot');
+  if (screenshots.length === 0) {
+    messageError(
+      'Trace contains no screenshots. Did you make sure screenshots were enabled before recording your trace?',
+    );
+    throw new Error();
+  }
+
+  messageInfo(
+    ' STARTING UP ',
+    'Checking environment and setting up params.',
+    colorYellow,
+  );
+
+  // setup our video
+  const framesPerSecond = args.frames || 60;
+  const msPerFrame = Math.round(1000000 / framesPerSecond);
+
+  // determine is this is a multipass
+  let multipassVideo = true;
+  if (args.hideClock && args.label !== '') {
+    multipassVideo = false;
+  }
+
+  // if we have no lable or clock, just dump the video
+  let stageOneFileOutputName;
+  if (multipassVideo) {
+    stageOneFileOutputName = tmp.tmpNameSync({
+      prefix: 'JDR-DEVTOOLS-',
+      postfix: '.mp4',
+    });
+  } else {
+    stageOneFileOutputName = args.output;
+  }
+
+  messageInfo(' CONVERT ', 'Spawning FFMPEG worker with open pipe.');
+  const videoPassOne = spawn(
+    'ffmpeg',
+    [
+      '-framerate',
+      `${framesPerSecond}`,
+      '-f',
+      'image2pipe',
+      '-vcodec',
+      'mjpeg',
+      '-i',
+      '-',
+      '-hide_banner',
+      '-loglevel',
+      'panic',
+      `${stageOneFileOutputName}`,
+    ],
+    {
+      stdio: ['pipe', 'inherit', 'inherit'],
+    },
+  );
+
+  // we may have to repeat the same screenshot in our video because nothing
+  // painted/changed during a specific period in the trace so we store a outside
+  // reference
+  let repeatBuffer;
+
+  let timestampTarget = screenshots[0].ts;
+
+  messageInfo(
+    ' CONVERT ',
+    'Begin piping screenshots from DevTools trace to FFMPEG.',
+  );
+
+  // Let's send some screenshots to the pipe This is largely a riff off of Kris
+  // Selden's trace-to-mp4.js gist (mad props)
+  // https://gist.github.com/krisselden/bf98fb0c192fcb73ed32e79c0a7972d2
+  for (const screenshot of screenshots) {
+    // loop frames until we've hit the next new frame
+    while (timestampTarget < screenshot.ts) {
+      videoPassOne.stdin.write(repeatBuffer);
+      timestampTarget += msPerFrame;
+    }
+
+    // new frame, cache it and write to the pipe
+    if (screenshot.args.snapshot !== '') {
+      repeatBuffer = Buffer.from(screenshot.args.snapshot, 'base64');
+      videoPassOne.stdin.write(repeatBuffer);
+    }
+    timestampTarget += msPerFrame;
+  }
+
+  // There is a case where the last frame is the main body render on some sites,
+  // which results in a strange early cut of the video (well, it just looks
+  // weird). Add 1 second of padding of the last frame
+  for (let index = 0; index < framesPerSecond; index++) {
+    videoPassOne.stdin.write(repeatBuffer);
+  }
+
+  // close the pipe
+  videoPassOne.stdin.end();
+
+  videoPassOne.on('close', () => {
+    if (multipassVideo) {
+      messageInfo(
+        ' SCREENSHOTS ',
+        `Processed ${screenshots.length} screenshots into video file.`,
+      );
+      messageInfo(' CONVERT ', 'DevTools trace successfully converted to MP4.');
+      messageInfo(' LABEL PASS ', 'Adding label and time scale to MP4.');
+      let secondDrawText = '';
+      if (args.label !== '') {
+        secondDrawText = `, drawtext=text='${args.label}':fontcolor=white:fontsize=18:x=(w-tw)/2:y=h-45-(th/2)`;
+      }
+
+      // This is a quick usage of filter_complex with presentation time to put
+      // in the bottom time scale. The downside is there is much control, but it
+      // gets the job done in a pinch. WebPageTest is more concise than I am:
+      // WPO-Foundation/webpagetest/blob/26e3cf0/www/video/render.php#L567
+      const videoPassTwo = spawn(
+        'ffmpeg',
+        [
+          '-i',
+          `${stageOneFileOutputName}`,
+          '-filter_complex',
+          `[0:v]pad=iw:ih+60:0:0:color=black, drawtext=text='%{pts\\:hms}':fontcolor=white:fontsize=24:x=(w-tw)/2:y=h-25-(th/2)${secondDrawText}`,
+          '-hide_banner',
+          '-loglevel',
+          'panic',
+          `${args.output}`,
+        ],
+        {
+          stdio: ['pipe', 'inherit', 'inherit'],
+        },
+      );
+
+      videoPassTwo.on('close', () => {
+        messageInfo(
+          ' LABEL PASS ',
+          'Finished adding label and time scale to MP4.',
+        );
+        messageComplete(args.output);
+      });
+    } else {
+      messageComplete(stageOneFileOutputName);
+    }
+  });
+};
+
+function messageComplete(filename) {
+  console.log(
+    `${colorGreen(
+      ' COMPLETE ',
+    )} DevTools trace to video is now complete! You file ${filename} is ready`,
+  );
+}
+
+function messageError(message) {
+  console.log(`${colorRed(' ERROR ')} ${message}`);
+}
+
+function messageInfo(heading, message, color = colorBlue) {
+  console.log(`${color(heading)} ${message}`);
+}
